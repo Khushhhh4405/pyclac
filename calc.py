f975d06b2d8bdbdb5ef6e8f0a1657ce4a654d9be
kban0019-commits <khushbandaria4405@gmail.com>
2026-04-02 03:03:10 +1100

    Use fancy-schmancy iterable unpacking

--- calc.py
+++ calc.py
@@ -21,3 +21,13 @@ def compute(expression):
     else:
         print('unknown operator!')
         return None
+    
+def compute(expression):
+    num0, operator, num1 = expression.split(' ')
+    if operator == '+':
+        return num0 + num1
+    elif operator == '-':
+        return num0 - num1
+    else:
+        print('unknown operator!')
+        return None
