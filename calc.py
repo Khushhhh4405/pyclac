8de9ad944e5860c13a613ab266434417a6688751
kban0019-commits <khushbandaria4405@gmail.com>
2026-04-02 03:12:06 +1100

    add support for multiplication operator

--- calc.py
+++ calc.py
@@ -1,39 +1,3 @@
-# Perform simple arithmetic encoded in an input string:
-# '1 + 2' -> 3, or '1 - 2' -> -1.
-def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
-    if operator == '+':
-        return num0 + num1
-    else:
-        print('unknown operator!')
-        return 0
-    
-def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
-    if operator == '+':
-        return num0 + num1
-    elif operator == '-':
-        return num0 - num1
-    else:
-        print('unknown operator!')
-        return None
-    
-def compute(expression):
-    num0, operator, num1 = expression.split(' ')
-    if operator == '+':
-        return num0 + num1
-    elif operator == '-':
-        return num0 - num1
-    else:
-        print('unknown operator!')
-        return None
-    
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
     num0, num1 = int(num0), int(num1)
@@ -41,6 +5,8 @@ def compute(expression):
         return num0 + num1
     elif operator == '-':
         return num0 - num1
+    elif operator == '*':
+        return num0 * num1
     else:
         print('unknown operator!')
         return None
