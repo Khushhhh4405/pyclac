57fe5829ce544096a9cfd03720d58f2880d95157
kban0019-commits <khushbandaria4405@gmail.com>
2026-04-02 03:06:02 +1100

    convert num strings to int

--- calc.py
+++ calc.py
@@ -33,3 +33,14 @@ def compute(expression):
     else:
         print('unknown operator!')
         return None
+    
+def compute(expression):
+    num0, operator, num1 = expression.split(' ')
+    num0, num1 = int(num0), int(num1)
+    if operator == '+':
+        return num0 + num1
+    elif operator == '-':
+        return num0 - num1
+    else:
+        print('unknown operator!')
+        return None
